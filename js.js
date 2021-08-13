a9e3db6a2a8c8c72135a8356c91626acb7def647
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-08-13 23:12:01 +0700

    responsive

--- js.js
+++ js.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $("#account-button").click(function() {
         $(".dropdown__hidden").toggle();
     })
+    $("#menu__menu--button").click(function() {
+        $(".sidebar").toggle();
+    })
 })
